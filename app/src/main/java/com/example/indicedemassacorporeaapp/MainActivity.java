d90dc4cab22a7686cccbaf78e1ce1584d65c3f7d
kleberfort <kleberfort@gmail.com>
2021-02-14 18:22:58 -0300

    Inserido uma caixa de mensagem quando clicamos no botao calcular e um Toast ao apertar o botão limpar

--- app/src/main/java/com/example/indicedemassacorporeaapp/MainActivity.java
+++ app/src/main/java/com/example/indicedemassacorporeaapp/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.indicedemassacorporeaapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -40,12 +42,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             double resultado = (peso/Math.pow(altura,2));
 
             edtResultado.setText(String.format("%.2f", resultado));
+
+            AlertDialog.Builder dlg = new AlertDialog.Builder(this);
+            dlg.setTitle(R.string.app_name);
+            dlg.setMessage(String.format("IMC= %.2f", resultado));
+            dlg.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+            dlg.show();
+
         }else{
             if(v.getId() == R.id.btnLimpar){
                 edtPeso.setText("");
                 edtAltura.setText("");
                 edtResultado.setText("");
                 edtPeso.requestFocus();
+
+                Toast.makeText(this,"Os dados foram apagagados com sucessso",
+                        Toast.LENGTH_LONG).show();
             }
         }
 
